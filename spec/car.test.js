aedfbcc51c53a49a346161967dc31380391f29b6
Ryan Jonathan <ryanjnthan@gmail.com>
2022-12-31 18:21:47 +0700

    add update delete

--- spec/car.test.js
+++ spec/car.test.js
@@ -1,5 +1,7 @@
 const app = require("../app/index");
 const request = require("supertest");
+const models = require("../app/models");
+const Car = models.Car;
 let token = "";
 
 beforeAll(async () => {
@@ -13,6 +15,14 @@ beforeAll(async () => {
   token = response.body.accessToken;
 });
 
+const payload = {
+  name: "Avanza Test",
+  price: 150000,
+  size: "medium",
+  image: "www.google.com",
+};
+
+// GET
 describe("GET /", () => {
   it("return 200 ok", (done) => {
     request(app).get("/").expect("Content-Type", "application/json; charset=utf-8").expect(200, done);
@@ -25,22 +35,75 @@ describe("GET /v1/cars", () => {
   });
 });
 
+describe("GET /v1/cars/:id", () => {
+  it("return 200 ok", async () => {
+    const car = await Car.create(payload);
+
+    await request(app).get(`/v1/cars/${car.id}`).expect("Content-Type", "application/json; charset=utf-8").expect(200);
+  });
+});
+
+// Create
 describe("POST /v1/cars", () => {
   it("return 201 created", async () => {
-    const payload = {
-      name: "Avanza Test",
-      price: 150000,
-      size: "medium",
-      image: "www.google.com",
-    };
-
     await request(app)
       .post("/v1/cars")
       .set({
         Authorization: `Bearer ${token}`,
       })
-      .set(payload)
+      .send(payload)
       .expect("Content-Type", "application/json; charset=utf-8")
       .expect(201);
+  });
+});
+
+// Update
+describe("PUT /v1/cars/:id", () => {
+  it("return 200 updated", async () => {
+    const payloadCreate = {
+      name: "Pajero",
+      price: 200000,
+      size: "Large",
+      image: "www.google.com",
+    };
+
+    const car = await Car.create(payloadCreate);
+
+    const payloadUpdate = {
+      name: "All New Pajero",
+      price: 230000,
+      size: "Large",
+      image: "www.google.com",
+    };
+
+    await request(app)
+      .put(`/v1/cars/${car.id}`)
+      .set({ Authorization: `Bearer ${token}` })
+      .send(payloadUpdate)
+      .expect("Content-Type", "application/json; charset=utf-8")
+      .expect(200);
+  });
+});
+
+// Delete
+
+describe("DELETE /v1/cars/:id", () => {
+  it("respond 204 deleted", async () => {
+    jest.setTimeout(60000);
+    const car = await Car.create({
+      name: "Brio",
+      price: 150000,
+      size: "small",
+      image: "www.google.com",
+      isCurrentlyRented: false,
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    });
+
+    await request(app)
+      .delete(`/v1/cars/${car.id}`)
+      .set({ Authorization: `Bearer ${token}` })
+      .expect("Content-Type", "application/json; charset=utf-8")
+      .expect(204);
   });
 });
