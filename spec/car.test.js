ffc52116f757f6502660684b220fb88cd73047be
Ryan Jonathan <ryanjnthan@gmail.com>
2022-12-31 17:00:48 +0700

    add post

--- spec/car.test.js
+++ spec/car.test.js
@@ -1,8 +1,46 @@
 const app = require("../app/index");
 const request = require("supertest");
+let token = "";
+
+beforeAll(async () => {
+  const credentialsLogin = {
+    email: "johnny@binar.co.id",
+    password: "123456",
+  };
+
+  const response = await request(app).post("/v1/auth/login").send(credentialsLogin);
+
+  token = response.body.accessToken;
+});
 
 describe("GET /", () => {
   it("return 200 ok", (done) => {
-    request(app).get("/").expect("Conten-Type", "application/json; charset=utf8").expect(200, done);
+    request(app).get("/").expect("Content-Type", "application/json; charset=utf-8").expect(200, done);
+  });
+});
+
+describe("GET /v1/cars", () => {
+  it("return 200 ok", (done) => {
+    request(app).get("/v1/cars").expect("Content-Type", "application/json; charset=utf-8").expect(200, done);
+  });
+});
+
+describe("POST /v1/cars", () => {
+  it("return 201 created", async () => {
+    const payload = {
+      name: "Avanza Test",
+      price: 150000,
+      size: "medium",
+      image: "www.google.com",
+    };
+
+    await request(app)
+      .post("/v1/cars")
+      .set({
+        Authorization: `Bearer ${token}`,
+      })
+      .set(payload)
+      .expect("Content-Type", "application/json; charset=utf-8")
+      .expect(201);
   });
 });
